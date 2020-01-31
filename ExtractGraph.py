ad811f0e1c8a0fb9b11a45feb788aecfc0b94c42
PowCoder <31611767+ecooler@users.noreply.github.com>
2020-01-31 13:56:47 +0800

    Add files via upload

---
+++ ExtractGraph.py
@@ -0,0 +1,16 @@
+class ExtractGraph:
+
+    # key is head word; value stores next word and corresponding probability.
+    graph = {}
+
+    sentences_add = "data\\assign1_sentences.txt"
+
+    def __init__(self):
+        # Extract the directed weighted graph, and save to {head_word, {tail_word, probability}}
+        return
+
+
+    def getProb(self, head_word, tail_word):
+        return 0.0
+
+
